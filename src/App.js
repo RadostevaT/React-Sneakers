dec121ae8dd584f7d3371800b58cbd1ed0356ba0
RadostevaT <radostevataisia@gmail.com>
2023-04-26 19:25:11 +0500

    Lesson 2.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,49 @@
 function App() {
   return (
     <div className="wrapper">
+      <div className="overlay">
+        <div className="drawer">
+          <h2>Корзина <img className="remove-btn" width={32} height={32} src="/img/button-remove.svg" alt="Закрыть корзину." /></h2>
+
+          <div className="cart-items">
+            <div className="cart-item">
+              <img width={70} height={70} src="/img/sneakers/1.jpg" alt="Кроссовки." />
+              <div className="cart-item__inner">
+                <p>Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img className="remove-btn" width={32} height={32} src="/img/button-remove.svg" alt="Удалить из корзины." />
+            </div>
+            <div className="cart-item">
+              <img width={70} height={70} src="/img/sneakers/2.jpg" alt="Кроссовки." />
+              <div className="cart-item__inner">
+                <p>Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img className="remove-btn" width={32} height={32} src="/img/button-remove.svg" alt="Удалить из корзины." />
+            </div>
+          </div>
+
+          <div className="drawer__bottom">
+            <ul>
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498 руб. </b>
+              </li>
+              <li>
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1074 руб. </b>
+              </li>
+            </ul>
+            <button className="button-green">Оформить заказ
+              <img src="/img/arrow.svg" alt="Arrow right." />
+              </button>
+          </div>
+        </div>
+      </div>
+
       <header className="header">
         <div className="header__title">
           <img width={40} height={40} src="/img/logo.png" alt="Логотип магазина." />
@@ -23,10 +66,19 @@ function App() {
         </ul>
       </header>
       <div className="catalog">
-        <h1>Все кроссовки</h1>
+        <div className="catalog__header">
+          <h1>Все кроссовки</h1>
+          <div className="search-block">
+            <img width={14} height={14} src="/img/search.svg" alt="Поиск." />
+            <input placeholder="Поиск ..." type="text" />
+          </div>
+        </div>
 
         <div className="catalog__wrapper">
           <div className="card">
+            <div className="favourite">
+              <img width={30} height={30} src="/img/heart-unliked.png" alt="Добавить в избранное." />
+            </div>
             <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Фотография пары кроссовок." />
             <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
             <div className="card__info">
@@ -35,7 +87,7 @@ function App() {
                 <b>12 999 руб.</b>
               </div>
               <button type="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="Добавить в корзину." />
+                <img width={32} height={32} src="/img/button-plus.svg" alt="Добавить в корзину." />
               </button>
             </div>
           </div>
@@ -49,7 +101,7 @@ function App() {
                 <b>12 999 руб.</b>
               </div>
               <button type="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="Добавить в корзину." />
+                <img width={32} height={32} src="/img/button-plus.svg" alt="Добавить в корзину." />
               </button>
             </div>
           </div>
@@ -63,7 +115,7 @@ function App() {
                 <b>12 999 руб.</b>
               </div>
               <button type="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="Добавить в корзину." />
+                <img width={32} height={32} src="/img/button-plus.svg" alt="Добавить в корзину." />
               </button>
             </div>
           </div>
@@ -77,7 +129,7 @@ function App() {
                 <b>12 999 руб.</b>
               </div>
               <button type="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="Добавить в корзину." />
+                <img width={32} height={32} src="/img/button-plus.svg" alt="Добавить в корзину." />
               </button>
             </div>
           </div>
